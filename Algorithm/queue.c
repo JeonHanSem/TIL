faa98b69988b89f7fe006335fd88c68a2002aae8
JeonHanSem <rmsdlf159@naver.com>
2016-05-09 19:53:30 +0900

    circle queue

---
+++ Algorithm/queue.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define max 100
+
+typedef struct{
+	int buf[max];
+	int front;
+	int rear;
+}Queue;
+
+Queue queue;
+
+void initStack(){
+	queue.front = -1;
+	queue.rear = -1;
+}
+
+void push(int n){
+	queue.buf[++queue.rear%max]=n;
+}
+
+int pop(){
+	return queue.buf[++queue.front%max];
+}
