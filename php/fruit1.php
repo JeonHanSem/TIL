120160964b844d1f96c834306a118d5fa5687f71
Sem <rmsdlf159@naver.com>
2016-09-02 17:32:19 +0900

    transfer value

---
+++ php/fruit1.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+	<form name=fruit method=post action="./curl.php">
+		Input New Data<br>
+		Sentence : <input type=text name=name><br>
+		<input type=submit value="Submit">
+		<input type=reset value="Reset">
+	</form>
+</body>
+</html>
